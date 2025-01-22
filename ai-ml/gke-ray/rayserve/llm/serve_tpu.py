f57aad85416510e591face33c2406408c1b9e988
Ryan O'Leary <ryanaoleary@google.com>
2025-01-22 18:25:05 +0000

    Revert serve_tpu
    Signed-off-by: Ryan O'Leary <ryanaoleary@google.com>

--- ai-ml/gke-ray/rayserve/llm/serve_tpu.py
+++ ai-ml/gke-ray/rayserve/llm/serve_tpu.py
@@ -43,7 +43,8 @@ class VLLMDeployment:
         dtype,
     ):
         self.llm = LLM(
-            model=os.environ["MODEL_ID"],
+            model="/data/Meta-Llama-3.1-405B-Instruct",
+            served_model_name="meta-llama/Meta-Llama-3.1-405B-Instruct",
             tensor_parallel_size=num_tpu_chips,
             max_model_len=max_model_len,
             dtype=dtype,
