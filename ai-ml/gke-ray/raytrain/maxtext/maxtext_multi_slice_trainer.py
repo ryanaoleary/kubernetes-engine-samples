592867eacd40546f9acea938ba6cd3e3119727a9
Ryan O'Leary <ryanaoleary@google.com>
2026-04-21 11:25:20 +0000

    Add MaxText multislice Ray Train samples with region tags
    Signed-off-by: Ryan O'Leary <ryanaoleary@google.com>

---
+++ ai-ml/gke-ray/raytrain/maxtext/maxtext_multi_slice_trainer.py
@@ -0,0 +1,66 @@
+# Copyright 2026 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# [START gke_ai_ml_gke_ray_ray_train_maxtext_maxtext_multi_slice_trainer]
+import os
+from absl import app
+import logging
+from typing import Sequence
+import ray
+from ray.train.v2.api.config import ScalingConfig, RunConfig
+from ray.train.v2.jax import JaxTrainer
+
+def train_loop_per_worker(config):
+    import maxtext
+    from maxtext.trainers.pre_train.train import main as maxtext_main
+
+    argv = config["argv"]
+    maxtext_main(argv)
+
+def main(argv: Sequence[str]):
+    # Convert the config file path to an absolute path
+    argv = list(argv)
+    if len(argv) > 1:
+        argv[1] = os.path.abspath(argv[1])
+
+    trainer = JaxTrainer(
+        train_loop_per_worker=train_loop_per_worker,
+        train_loop_config={"argv": argv},
+        scaling_config=ScalingConfig(
+            use_tpu=True,
+            num_workers=8,
+            topology="4x4",
+            accelerator_type="TPU-V6E",
+            resources_per_worker={"TPU": 4},
+            placement_strategy="SPREAD",
+        ),
+        run_config=RunConfig(
+            name="maxtext_jaxtrainer",
+            worker_runtime_env={
+                "uv": {
+                    # maxtext requires some additional deps
+                    "packages": ["maxtext[tpu]==0.2.1"],
+                    "uv_pip_install_options": ["--resolution=lowest"]
+                },
+            },
+        ),
+    )
+    result = trainer.fit()
+    logging.info("Training complete!")
+    ray.shutdown()
+
+if __name__ == "__main__":
+    app.run(main)
+
+# [END gke_ai_ml_gke_ray_ray_train_maxtext_maxtext_multi_slice_trainer]
